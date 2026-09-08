3b92bc81f9323016538e8448cc9bd03a79c0e233
DrmmrMik <DrmmrMik@users.noreply.github.com>
2026-09-08 09:27:57 -0400

    deploy: update penandpaper PWA assets

--- penandpaper/registerSW.js
+++ penandpaper/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pwas/penandpaper/sw.js', { scope: '/pwas/penandpaper/' })})}
